e5c386a8463c1f7cdd7d155b03d2e77b79bb0804
tomevans18 <thomasevans18@gmail.com>
2019-04-17 10:27:31 +0100

    added spec for logged in/out onclick and active link styling

--- components/Header/Header.spec.jsx
+++ components/Header/Header.spec.jsx
@@ -59,4 +59,70 @@ describe('Header', () => {
 
     expect(toJson(wrapper)).toMatchSnapshot();
   });
+
+  it('should run logout function when button is clicked and user is logged in', () => {
+    const loginMockFunc = jest.fn();
+    const logoutMockFunc = jest.fn();
+    mockUserContextContext.mockReturnValue({
+      isLoggedIn: true,
+      userName: 'Tom Evans',
+      avatar: 'http://example.com',
+      status: 'Available',
+      login: loginMockFunc,
+      logout: logoutMockFunc,
+    });
+    const wrapper = wrapWithTheme(mount, <Header mainId="#abc123" />);
+
+    wrapper
+      .find('button')
+      .last()
+      .props()
+      .onClick();
+
+    expect(loginMockFunc).toHaveBeenCalledTimes(0);
+    expect(logoutMockFunc).toHaveBeenCalledTimes(1);
+  });
+
+  it('should run login function when button is clicked and user is logged out', () => {
+    const loginMockFunc = jest.fn();
+    const logoutMockFunc = jest.fn();
+    mockUserContextContext.mockReturnValue({
+      isLoggedIn: false,
+      userName: null,
+      status: 'Available',
+      avatar: null,
+      protectedPage: false,
+      login: loginMockFunc,
+      logout: logoutMockFunc,
+    });
+    const wrapper = wrapWithTheme(mount, <Header mainId="#abc123" />);
+
+    wrapper
+      .find('button')
+      .last()
+      .props()
+      .onClick();
+
+    expect(loginMockFunc).toHaveBeenCalledTimes(1);
+    expect(logoutMockFunc).toHaveBeenCalledTimes(0);
+  });
+
+  it('should render with profile link in bold', () => {
+    mockRouterContext.mockReturnValue({
+      router: {
+        pathname: '/profile',
+      },
+    });
+    mockUserContextContext.mockReturnValue({
+      isLoggedIn: true,
+      userName: 'Tom Evans',
+      avatar: 'http://example.com',
+      status: 'Available',
+      login: () => {},
+      logout: () => {},
+    });
+    const wrapper = wrapWithTheme(mount, <Header mainId="#abc123" />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
